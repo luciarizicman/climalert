84a3c971e1a45e2a203b2ac1997f1e056a1c1431
luciarizicman <luciarizicman@hotmail.com>
2026-07-06 21:00:36 -0300

    Proyecto Spring Boot inicial

---
+++ src/test/java/com/lucia/climalert/ClimalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucia.climalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
